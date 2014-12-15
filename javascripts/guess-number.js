658be3ff3e9e77d08b180c2794747bad66299b9f
JonaMata <jonathanmatarazzi@gmail.com>
2014-12-15 17:44:48 +0100

    Bug Fixes
    Fixed some so it will say “invalid choice” if your choice is invalid
    and so it will cancel the game when your press cancel.

--- javascripts/guess-number.js
+++ javascripts/guess-number.js
@@ -1,25 +1,47 @@
 var number = Math.floor((Math.random() * 100) + 1);
-document.write("Guess the number!<br><br>");
+document.write("Guess the number!   ");
 var loop = (0);
 while (loop == 0) {
-    var guess = prompt("What do you guess?");
-    if (guess > number) {
-        alert("Lower!");
+    var guess = prompt("What do you guess? Choose a number from 1 to 100.");
+    if (guess == null) {
+        loop = (1);
+        document.write("You canceled the game.")
     }
-    if (guess < number) {
-        alert("Higher!");
-    }
-    if (guess == number) {
+    else {
+        if (guess == number) {
         alert("Correct!");
         loop = (1);
+        document.write("You were correct, the answer was ");
+        document.write(number);
+        document.write("!");
         }
-    if (guess > 100) {
-        alert("Invalid choice")
+    else {
+        if (guess % 1 != 0) {
+            alert("Invalid choice, you must choose a number from 1 to 100!");
+        }
+            else {
+        if (guess > number && guess < 101) {
+            alert("Lower!");
+        }
+        else {
+            if (guess < number && guess > 0) {
+                alert("Higher!");
+            }
+            else {
+                if (guess > 100) {
+                    alert("Invalid choice, you must choose a number from 1 to 100!")
+                }
+                else {
+                    if (guess < 1) {
+                        alert("Invalid choice, you must choose a number from 1 to 100!")
+                    }
+                    else {
+                        alert("Invalid choice, you must choose a number from 1 to 100!")
+                    }
+                }
+            }
+        }
     }
-    if (guess < 1) {
-        alert("Invalid choice")
+            }
+        }
     }
-}
-document.write("You were correct, the answer was ");
-document.write(number);
-document.write("!");
